a63598e8cc9ce689039e9915b05e6fb2a3642fe5
bubersson <bubersson@gmail.com>
2020-02-08 13:26:16 -0800

    Make go binaries executable globally

--- paths.sh
+++ paths.sh
@@ -1,5 +1,9 @@
 export GOPATH=$HOME/src/go
 
+# Allow go programs being executable globally. 
+export PATH=$PATH:$GOPATH/bin
+
+# Allow my scripts to be executed globally
 export PATH=$PATH:~/init/scripts/
 
 export GIT_TERMINAL_PROMPT=1
