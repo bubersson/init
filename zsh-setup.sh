a9582ab761d74e5ac85a3db47f62ccfa2c787a3a
bubersson <bubersson@gmail.com>
2020-06-30 18:17:45 -0700

    Add ZSH setup and a theme

---
+++ zsh-setup.sh
@@ -0,0 +1,6 @@
+# For zsh specific setups
+
+# Make the command left and right work with iTerm2
+# https://coderwall.com/p/a8uxma/zsh-iterm2-osx-shortcuts
+bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
