326ee44373585dae14ecefbc293303a4052caa53
bubersson <bubersson@gmail.com>
2020-08-20 20:49:50 -0700

    Add GOROOT and update GOPATH to be ~/go

--- paths.sh
+++ paths.sh
@@ -1,10 +1,14 @@
 ## Set up global paths
 
+# GOROOT is the location where the Go package is installed on your system.
+export GOROOT=/usr/local/go
+
 # Set GOPATH (otherwise defaults to $HOME/go)
-export GOPATH=$HOME/src/go
+# GOPATH is the work directory of your go project.
+export GOPATH=$HOME/go
 
 # Allow go programs being executable globally. 
-export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
 
 # Allow my scripts to be executed globally
 export PATH=$PATH:~/init/scripts/
