5bf007ba0ad43152be7403b3f7d754bcf0df16aa
bubersson <bubersson@gmail.com>
2020-09-25 19:29:19 -0700

    Add command to link go binary

--- paths.sh
+++ paths.sh
@@ -1,7 +1,10 @@
 ## Set up global paths
 
 # GOROOT is the location where the Go package is installed on your system.
-# export GOROOT=/usr/local/go
+#   export GOROOT=/usr/local/go
+# Or create:
+#   ln -s /usr/local/go/bin/go /usr/local/bin/go
+
 
 # Set GOPATH (otherwise defaults to $HOME/go)
 # GOPATH is the work directory of your go project.
